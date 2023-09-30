dd9251a2218e583ae8484315fb72b93ad608358d
Your Name <you@example.com>
2023-09-30 07:00:37 -0400

    Added usart example program

---
+++ src/examples/usart.cpp
@@ -0,0 +1,28 @@
+/*
+This program is an example of using USART to send and receive serial data.
+*/
+
+#include <stdint.h>
+
+#include <avr_cpp/usart0.h>
+
+using namespace avr_cpp;
+
+constexpr auto BAUD_RATE = 9600;
+
+int main() {
+    Usart0 usart(Usart::DataSize::EightBits,
+                 Usart::NumStopBits::One, 
+                 BAUD_RATE);
+
+    uint8_t input;
+
+    while (true) {
+        usart << "Enter a character: ";
+        usart >> input;
+
+        usart << "\nReceived: " << input << '\n';
+    }
+
+    return 0;
+}
